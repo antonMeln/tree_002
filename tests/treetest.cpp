42f71b6b9cea3924a10fddbb15052f2dc4879134
antonMeln <32076906+antonMeln@users.noreply.github.com>
2018-06-24 22:38:24 +0300

    Create treetest.cpp

---
+++ tests/treetest.cpp
@@ -0,0 +1,89 @@
+#include <catch.hpp>
+#include "tree.cpp"
+
+TEST_CASE("insert and remove")
+{
+  std::string input1{
+      "------------14\n"
+      "--------12\n"
+      "------------11\n"
+      "----10\n"
+      "------------9\n"
+      "--------7\n"
+      "------------6\n"
+      "--------------------4\n"
+      "----------------3\n"};
+  std::string input2{
+      "------------14\n"
+      "--------12\n"
+      "------------11\n"
+      "----10\n"
+      "------------9\n"
+      "--------7\n"
+      "----------------4\n"
+      "------------3\n"};
+  tree_t<int> Tree;
+  
+  Tree.insert(10);
+  Tree.insert(12);
+  Tree.insert(11);
+  Tree.insert(14);
+  Tree.insert(7);
+  Tree.insert(9);
+  Tree.insert(6);
+  Tree.insert(3);
+  Tree.insert(4);
+  
+  std::ostringstream ostream;
+  Tree.print( ostream );
+  
+  REQUIRE( input1 == ostream.str() );
+  
+  REQUIRE( Tree.remove(6) == true);
+  Tree.print( ostream );
+  
+  REQUIRE( input1 + input2 == ostream.str() );
+}
+
+
+TEST_CASE("Find_")
+{
+  tree_t<int> Tree{10,12,11,14,7,9,6,3,4};
+  
+  REQUIRE( Tree.find(6) == true );
+  REQUIRE( Tree.find(1) == false );
+}
+
+
+TEST_CASE("Initializer_list_")
+{
+   std::string input{
+      "------------14\n"
+      "--------12\n"
+      "------------11\n"
+      "----10\n"
+      "------------9\n"
+      "--------7\n"
+      "------------6\n"
+      "--------------------4\n"
+      "----------------3\n"};
+  tree_t<int> Tree{10,12,11,14,7,9,6,3,4};
+  
+  std::ostringstream ostream;
+  Tree.print( ostream );
+  
+  REQUIRE( input == ostream.str() );
+}
+
+
+TEST_CASE("operator ==")
+{
+  tree_t<int> Tree1{10,12,11,14,7,9,6,3,4};
+  tree_t<int> Tree2{10,12,11,14,7,9,6,3,4};
+  
+  REQUIRE( (Tree1 == Tree2) == true );
+  
+  Tree2.remove(6);
+  
+  REQUIRE( (Tree1 == Tree2) == false );
+}
